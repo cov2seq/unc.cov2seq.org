90abeb4f44ac3228171e3a848ffe1f27d9f1ff66
txje <jeremyrwang@gmail.com>
2022-03-22 14:53:01 -0400

    initial commit

---
+++ main_simple.js
@@ -0,0 +1,303 @@
+let voi = {
+  "B.1.1.7": "Alpha",
+  "B.1.351": "Beta",
+  "P.1":"Gamma",
+  "B.1.617.2":"Delta",
+  "AY.*": "Delta",
+  "B.1.427":"Epsilon",
+  "B.1.429":"Epsilon",
+  "P.2":"Zeta",
+  "B.1.525":"Eta",
+  "P.3": "Theta",
+  "B.1.526":"Iota",
+  "B.1.526.1":"Iota",
+  "B.1.526.2":"Iota",
+  "B.1.526.3":"Iota",
+  "B.1.617.1":"Kappa",
+  "B.1.621": "Mu",
+  //"B.1.1.529": "Omicron", // nothing can be assigned here anymore, according to pangolin, all will be BA.*
+  "BA.1": "Omicron",
+  "BA.1.1": "Omicron",
+  "BA.2": "Omicron (BA.2)",
+  "BA.3": "Omicron"
+}
+
+// total VOC used to be "#00FF00",
+let cl = ["#AAAAAA", "#00FFFF", "#776600", "#880000", "#FF00FF", "#FF0000", "#FF99AA", "#FFFF00", "#FF7700", "#FFAA33", "#000000"];
+//        Other      Alpha      Beta       Gamma      Delta      Epsilon    Iota       Mu         Omicron    BA.2
+
+function loadText(file, callback) {   
+  var xobj = new XMLHttpRequest();
+  //xobj.overrideMimeType("application/json");
+  xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);  
+}
+
+function loadJSON(file, callback) {   
+  loadText(file, function(content) {
+    console.log(content);
+    callback(JSON.parse(content));
+  });
+}
+
+// GLOBALS
+let phylo = null; // parsed Newick tree
+let meta = null; // metadata
+
+function init() {
+
+  loadText("data/JW0001_5556.meta_simple.tsv", function(content) {
+    temp = [];
+    lines = content.trim().split('\n');
+    header = lines[0].split('\t');
+    for(var i = 1; i < lines.length; i++) {
+      parts = lines[i].split('\t');
+      temp[parts[0]] = [];
+      for(var j = 0; j < parts.length; j++) {
+        temp[parts[0]][header[j]] = parts[j];
+      }
+    }
+    meta = temp; // we use the temp to reduce the time that 'meta' is not null such that check_var() could come up true without meta being fully populated
+    trends(meta, "variants", "voc");
+  });
+}
+
+function trends(meta, eid2, eid) {
+
+  // parse weeks, find maximum week
+  // 2021 week 1 start with Jan 4 (Monday)
+  let max_week = 0;
+  for(sample in meta) {
+    if(parseInt(meta[sample]["isoweek"]) > max_week) {
+      max_week = parseInt(meta[sample]["isoweek"]);
+    }
+  }
+  let dates = [];
+  let st = new Date(2021, 0, 4, 12); // Jan 4 at 12pm
+  for(var w = 0; w < max_week; w++) {
+    let en = new Date(st.getTime());;
+    en.setDate(en.getDate() + 6);
+    dates.push((st.getYear()-100) + "-" + (st.getMonth()+1) + "-" + st.getDate() + " > " + (en.getYear()-100) + "-" + (en.getMonth()+1) + "-" + en.getDate());
+    st.setDate(st.getDate() + 7);
+  }
+
+  let lineages = {}; //pango lineage
+  cts = new Array(dates.length).fill(0);
+  for(id in meta) {
+    let l = meta[id]["lineage"];
+    if(!(l in lineages)) {
+      lineages[l] = new Array(dates.length);
+      for(i = 0; i < lineages[l].length; i++) {
+        lineages[l][i] = 0;
+      }
+    }
+
+    let w = parseInt(meta[id]["isoweek"]) - 1; // offset by 1 to make it 0-indexed
+    cts[w]++;
+    if(isNaN(w)) {console.log(id, w, l, "MISSING WEEK!"); continue;} // it didn't have a week for some reason (ex. 1466)
+
+    lineages[l][w]++;
+  }
+
+  let table = document.createElement("table");
+  document.getElementById(eid).appendChild(table);
+  let tr = document.createElement("tr");
+  table.appendChild(tr);
+  let col = document.createElement("th");
+  col.innerText = "WHO";
+  tr.appendChild(col);
+  col = document.createElement("th");
+  col.innerText = "Pango lineage";
+  tr.appendChild(col);
+  col = document.createElement("th");
+  col.innerText = "# Samples";
+  tr.appendChild(col);
+
+  for(v in voi) {
+    tr = document.createElement("tr");
+    table.appendChild(tr);
+    col = document.createElement("td");
+    col.innerText = voi[v];
+    tr.appendChild(col);
+    col = document.createElement("td");
+    col.innerText = v;
+    tr.appendChild(col);
+    col = document.createElement("td");
+    let tot = 0;
+    if(v == "AY.*") {
+      for(l in lineages) {
+        if(l.indexOf("AY") != -1) {
+          for(j = 0; j < lineages[l].length; j++)
+            tot += lineages[l][j];
+        }
+      }
+    } else if(v in lineages) {
+      for(j = 0; j < lineages[v].length; j++)
+        tot += lineages[v][j];
+    }
+    col.innerText = tot;
+    tr.appendChild(col);
+  }
+
+  let tot_all = 0;
+  let voc_all = 0;
+  let non_voi = 0;
+  for(l in lineages) {
+    for(j = 0; j < lineages[l].length; j++) {
+      tot_all += lineages[l][j];
+      if(l in voi || l.indexOf("AY") != -1) {
+        voc_all += lineages[l][j];
+      } else {
+        non_voi += lineages[l][j];
+      }
+    }
+  }
+  tr = document.createElement("tr");
+  table.appendChild(tr);
+  tr.appendChild(document.createElement("th"));
+  col = document.createElement("th");
+  col.innerText = "Other (non-VOI/VOC)";
+  tr.appendChild(col);
+  col = document.createElement("td");
+  col.innerText = non_voi;
+  tr.appendChild(col);
+
+  tr = document.createElement("tr");
+  table.appendChild(tr);
+  tr.appendChild(document.createElement("th"));
+  col = document.createElement("th");
+  col.innerText = "Total samples";
+  tr.appendChild(col);
+  col = document.createElement("td");
+  col.innerText = tot_all;
+  tr.appendChild(col);
+
+  document.getElementById(eid).appendChild(document.createElement("br"));
+  document.getElementById(eid).appendChild(document.createElement("br"));
+
+  // VOC % by week
+  table = document.createElement("table");
+  document.getElementById(eid).appendChild(table);
+  tr = document.createElement("tr");
+  table.appendChild(tr);
+  col = document.createElement("th");
+  col.innerText = "Week";
+  tr.appendChild(col);
+  col = document.createElement("th");
+  col.innerText = "# Samples";
+  tr.appendChild(col);
+
+  let wk_voc = new Array(dates.length).fill(0);
+  let wk_tot = new Array(dates.length).fill(0);
+  for(l in lineages) {
+    for(j = 0; j < lineages[l].length; j++) {
+      wk_tot[j] += lineages[l][j];
+      if(l in voi || l.indexOf("AY") != -1) {
+        wk_voc[j] += lineages[l][j];
+      }
+    }
+  }
+  for(i = 0; i < dates.length; i++) {
+    tr = document.createElement("tr");
+    table.appendChild(tr);
+    col = document.createElement("th");
+    col.innerText = dates[i];
+    tr.appendChild(col);
+    col = document.createElement("td");
+    col.innerText = wk_tot[i];
+    tr.appendChild(col);
+  }
+
+  apex = []
+
+  // total VOI/C
+  y = new Array(dates.length).fill(0);
+	for(l in lineages) {
+    if((!(l in voi) && l.indexOf("AY") == -1)) {
+      for(i = 0; i < lineages[l].length; i++) {
+        y[i] += lineages[l][i];
+      }
+    }
+  }
+  for(var j = 0; j < y.length; j++) {
+    y[j] = y[j] / cts[j] * 100;
+  }
+  apex.push({"name":"Other", "data":y});
+
+  variants = {}; // a dictionary of <variant> : [list of <pango lineage>]
+  for(pango in voi) {
+    if(!(voi[pango] in variants)) {
+      variants[voi[pango]] = [];
+    }
+    variants[voi[pango]].push(pango);
+  }
+
+	for(v in variants) {
+    if(["-", "Zeta", "Theta", "Kappa", "Eta"].indexOf(v) != -1) continue;
+    y = new Array(dates.length).fill(0);
+    for(l in lineages) {
+      if(variants[v].indexOf(l) != -1 || (v == "Delta" && l.indexOf("AY") != -1)) {
+        for(i = 0; i < lineages[l].length; i++) {
+          y[i] += lineages[l][i];
+        }
+      }
+    }
+		for(var j = 0; j < y.length; j++) {
+			y[j] = y[j] / cts[j] * 100;
+		}
+    apex.push({"name":v, "data":y});
+	}
+
+  let end_dates = [];
+  for(i = 0; i < dates.length; i++) {
+    end_dates.push(dates[i].substr(dates[i].indexOf(">")+2));
+  }
+  var options = {
+    colors: cl,
+    chart: {
+      type: 'line',
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    yaxis: {
+      labels: {
+        formatter: function (value) {
+          return parseInt(value);
+        }
+      },
+      min: 0,
+      max: 100,
+      title: {
+        text: "% incidence"
+      }
+    },
+    series: apex,
+    xaxis: {
+      categories: end_dates
+    },
+    tooltip: {
+      y: {
+        formatter: function(value, { series, seriesIndex, dataPointIndex, w }) {
+          return value.toFixed(1)+" %";
+        }
+      }
+    }
+  }
+
+  document.getElementById(eid2).innerHTML = '';
+  var chart = new ApexCharts(document.querySelector('#'+eid2), options)
+  chart.render()
+}
+
+
+// fire init() when page is loaded (i.e. body.onload...)
+if (document.addEventListener) {
+  document.addEventListener("DOMContentLoaded", init, false);
+}
+
