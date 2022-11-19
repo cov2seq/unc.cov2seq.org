f726b95b6948b83d8860a00dac82eba52207ef12
txje <jeremyrwang@gmail.com>
2022-11-18 19:11:46 -0500

    221118

--- main_simple.js
+++ main_simple.js
@@ -66,9 +66,9 @@ let meta = null; // metadata
 
 function init() {
 
-  document.getElementById("report_date").innerText = "November 6, 2022";
-  document.getElementById("sample_date").innerText = "October 25, 2022";
-  loadText("221106.meta_simple.tsv", function(content) {
+  document.getElementById("report_date").innerText = "November 18, 2022";
+  document.getElementById("sample_date").innerText = "November 9, 2022";
+  loadText("221118.meta_simple.tsv", function(content) {
     temp = [];
     lines = content.trim().split('\n');
     header = lines[0].split('\t');
