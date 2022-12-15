ac0a3b5b9af7d70bbae6766c916e5a38f6118a96
txje <jeremyrwang@gmail.com>
2022-12-15 13:42:09 -0500

    221215

--- main_dynamic.js
+++ main_dynamic.js
@@ -314,9 +314,9 @@ function loadJSON(file, callback) {
 let meta = null; // metadata
 
 function init() {
-  document.getElementById("report_date").innerText = "December 9, 2022";
-  document.getElementById("sample_date").innerText = "November 30, 2022";
-  loadText("221209.meta_simple.tsv", function(content) {
+  document.getElementById("report_date").innerText = "December 15, 2022";
+  document.getElementById("sample_date").innerText = "December 7, 2022";
+  loadText("221215.meta_simple.tsv", function(content) {
     temp = [];
     lines = content.trim().split('\n');
     header = lines[0].split('\t');
