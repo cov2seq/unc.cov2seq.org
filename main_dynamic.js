d62cca24a73f5fb509bc1bf82e2a4740d724dd75
txje <jeremyrwang@gmail.com>
2023-03-01 12:51:55 -0500

    230301

--- main_dynamic.js
+++ main_dynamic.js
@@ -326,9 +326,9 @@ function loadJSON(file, callback) {
 let meta = null; // metadata
 
 function init() {
-  document.getElementById("report_date").innerText = "February 15, 2023";
-  document.getElementById("sample_date").innerText = "February 5, 2023";
-  loadText("230215.meta_simple.tsv", function(content) {
+  document.getElementById("report_date").innerText = "March 1, 2023";
+  document.getElementById("sample_date").innerText = "February 18, 2023";
+  loadText("230301.meta_simple.tsv", function(content) {
     temp = [];
     lines = content.trim().split('\n');
     header = lines[0].split('\t');
